213b19497e55735bb09188e166d01145dc646514
Ujsharma <ujwalsharma.s@gmail.com>
2024-08-08 18:08:32 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -5,9 +5,9 @@ char size(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms >= 38 && cms < 42) { // Handle the case where cms == 38
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if(cms >= 42) { // Changed from > 42 to >= 42
         sizeName = 'L';
     }
     return sizeName;
@@ -15,7 +15,9 @@ char size(int cms) {
 
 int main() {
     assert(size(37) == 'S');
+    assert(size(38) == 'M'); // Added test for cms == 38
     assert(size(40) == 'M');
+    assert(size(42) == 'L'); // Added test for cms == 42
     assert(size(43) == 'L');
     std::cout << "All is well (maybe!)\n";
     return 0;
