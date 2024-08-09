2465035c0cdee92b0ce70f274c160ff67caf008e
Ujsharma <ujwalsharma.s@gmail.com>
2024-08-09 15:50:27 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -12,20 +12,13 @@ char size(int cms) {
     }
     return sizeName;
 }
-
-void test_size() {
-    // Test cases
-    assert(size(37) == 'S');  // cms < 38, should be 'S'
-    assert(size(38) == 'M');  // cms >= 38 and < 42, should be 'M'
-    assert(size(40) == 'M');  // cms >= 38 and < 42, should be 'M'
-    assert(size(42) == 'L');  // cms >= 42, should be 'L'
-    assert(size(43) == 'L');  // cms >= 42, should be 'L'
-    // Add test for boundary case
-    assert(size(41) == 'M');  // This should fail if there's an issue with boundary condition
 }
 
 int main() {
-    test_size();
-    std::cout << "All is well (maybe!)\n";
+    assert(size(37) == 'S');
+    assert(size(38) == 'M');
+    assert(size(40) == 'M');
+    assert(size(43) == 'L');
+    printf("All is well (maybe!)\n");
     return 0;
 }
